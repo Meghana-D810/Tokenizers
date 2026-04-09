05671200230c4734ac7ac1e4881c3a43855f172a
Pranav <pranavai3513@gmail.com>
2026-04-09 15:55:22 +0530

    Pushed refined tokenizer.py

--- backend/tokenizer.py
+++ backend/tokenizer.py
@@ -3,19 +3,13 @@ import tiktoken
 
 
 def tokenize_text(text, model="gpt-4o-mini"):
-
-    # -------- Word Tokens (for heatmap & scoring) --------
     
     word_tokens = re.findall(r"\b\w+\b", text)
     word_token_count = len(word_tokens)
-
-
-    # -------- LLM Tokens (for cost calculation) --------
     
-    encoding = tiktoken.get_encoding("cl100k_base")
+    encoding = tiktoken.encoding_for_model(model)
     llm_tokens = encoding.encode(text)
     llm_token_count = len(llm_tokens)
-
 
     return {
         "word_tokens": word_tokens,
